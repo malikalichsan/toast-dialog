0333c6f588277a304009e26a68531b7e54190a4c
Malik Al Ichsan <malik.al.ichsan@gmail.com>
2018-01-10 11:58:20 +0700

    update string app name

--- app/src/main/java/com/learnandroid/toastdialog/MainActivity.java
+++ app/src/main/java/com/learnandroid/toastdialog/MainActivity.java
@@ -1,22 +1,31 @@
-  package com.learnandroid.toastdialog;
+package com.learnandroid.toastdialog;
 
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
   public class MainActivity extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Context context = getApplicationContext();
-        CharSequence text = "Hello Toast";
-        int duration = Toast.LENGTH_SHORT;
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context = getApplicationContext();
+                CharSequence text = "Hello Toast";
+                int duration = Toast.LENGTH_LONG;
 
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
     }
 }
